426361727cc15e7dfdb2c0ca96bbae0a2b579502
wissalbelhadjamorr <wissalbelhadjamor12@gmail.com>
2025-01-04 18:09:40 +0100

    Update api.service.ts

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,4 +1,3 @@
-// src/app/shared/api.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -10,15 +9,10 @@ import { Produit } from './produit.modal';
 export class ApiService {
 
   private apiUrl = 'http://localhost:3000/products';  
-
   constructor(private http: HttpClient) { }
-
-  // Récupérer tous les produits
   getProducts(): Observable<Produit[]> {
     return this.http.get<Produit[]>(this.apiUrl);
   }
-
-  // Ajouter un produit
   addProduct(product: Produit): Observable<Produit> {
     return this.http.post<Produit>(this.apiUrl, product);  
   }
