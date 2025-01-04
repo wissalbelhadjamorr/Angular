b4e4fe16dbbb8ed336bc759ec0c99560083d8264
wissalbelhadjamorr <wissalbelhadjamor12@gmail.com>
2025-01-04 18:11:28 +0100

    Update gestion-produits.component.ts

--- src/app/gestion-produits/gestion-produits.component.ts
+++ src/app/gestion-produits/gestion-produits.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../shared/api.service';  // Import du service pour récupérer les produits
-import { Produit } from '../shared/produit.modal';  // Modèle Produit
+import { ApiService } from '../shared/api.service'; 
+import { Produit } from '../shared/produit.modal';  
 
 @Component({
   selector: 'app-gestion-produits',
@@ -15,10 +15,10 @@ export class GestionProduitsComponent implements OnInit {
 
   ngOnInit(): void {
     this.apiService.getProducts().subscribe((data: Produit[]) => {
-      console.log('Produits récupérés:', data);  // Vérifiez que les données sont bien reçues
+      console.log('Produits récupérés:', data); 
       this.produits = data;
     }, error => {
-      console.error('Erreur lors de la récupération des produits:', error);
+
     });
   }
 }
